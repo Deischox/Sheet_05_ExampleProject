98d2609708640b4b075c741f9d2f214f1329db95
Deischox <silas.ueberschaer@gmx.de>
2023-05-24 17:12:16 +0200

    Changes

--- hamburg/dbis/recovery/RecoveryManager.java
+++ hamburg/dbis/recovery/RecoveryManager.java
@@ -3,10 +3,13 @@ package hamburg.dbis.recovery;
 import hamburg.dbis.persistence.PersistenceManager;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class RecoveryManager {
 
     static final private RecoveryManager _manager;
+    private Hashtable<Integer, Integer> data;
 
     // TODO Add class variables if necessary
     static {
@@ -19,56 +22,98 @@ public class RecoveryManager {
 
     private RecoveryManager() {
         // TODO Initialize class variables if necessary
+        data = new Hashtable<>();
     }
 
     static public RecoveryManager getInstance() {
         return _manager;
     }
 
+    public void startRecoveryPrePhase() {
+
+        // Iterate over the log entries and apply the necessary recovery operations
+        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] entry = line.split(",");
+                String operation = entry[2];
+                int transactionID = Integer.parseInt(entry[1]);
+                String LSN = entry[0];
+                if (operation.equals("EOT")) {
+                    data.put(transactionID, Integer.parseInt(LSN));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void startRecovery() {
-       // TODO Perform recovery
-       int lastTransactionID = retrieveLastUsedTransactionID();
+        startRecoveryPrePhase();
+        startRecoveryPhaseTwo();
+        data = new Hashtable<>();
+    }
 
-       // Iterate over the log entries and apply the necessary recovery operations
-       try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
-           String line;
-           while ((line = reader.readLine()) != null) {
-               String[] entry = line.split(",");
-               int transactionID = Integer.parseInt(entry[0]);
-               String operation = entry[2];
-               String LSN = entry[0];
+    public void startRecoveryPhaseTwo() {
+        // TODO Perform recovery
+        int lastTransactionID = retrieveLastUsedTransactionID();
+        int lastUsedLSN = retrieveLastUsedLSN();
+        int lastUsedLSNBeforeRecovery = lastUsedLSN;
 
-               if (transactionID <= lastTransactionID) {
-                   // Apply recovery operation based on the operation type
-                   if (operation.equals("EOT")) {
-                       // Handle end-of-transaction
-                       // Perform necessary recovery actions
-                       performEndOfTransactionRecovery(transactionID);
-                   } else {
-                       // Handle data modification operation
-                       int pageID = Integer.parseInt(entry[2]);
-                       String data = LSN + "," + entry[3];
-                       // Perform necessary recovery actions for data modification
-                       performDataModificationRecovery(transactionID, pageID, data);
-                   }
-               }
-           }
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
+        // Iterate over the log entries and apply the necessary recovery operations
+        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] entry = line.split(",");
+                int transactionID = Integer.parseInt(entry[1]);
+                String operation = entry[2];
+                String LSN = entry[0];
+
+                if (transactionID <= lastTransactionID && Integer.parseInt(LSN) <= lastUsedLSNBeforeRecovery
+                        && data.get(transactionID) != null && data.get(transactionID) >= Integer.parseInt(LSN)) {
+                    lastUsedLSN++;
+                    // Apply recovery operation based on the operation type
+                    if (operation.equals("EOT")) {
+                        // Handle end-of-transaction
+                        // Perform necessary recovery actions
+                        writeLogEntry(generateLogEntry(lastUsedLSN, transactionID, "EOT"));
+                        performEndOfTransactionRecovery(transactionID);
+                    } else {
+                        // Handle data modification operation
+                        int pageID = Integer.parseInt(entry[2]);
+                        String data = lastUsedLSN + "," + entry[3];
+                        writeLogEntry(generateLogEntry(lastUsedLSN, transactionID, pageID, data));
+                        // Perform necessary recovery actions for data modification
+                        performDataModificationRecovery(transactionID, pageID, data);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateLogEntry(int lastUsedLSN, int transactionID, int pageID, String data) {
+        return lastUsedLSN + "," + transactionID + "," + pageID + "," + data;
+    }
+
+    private String generateLogEntry(int lastUsedLSN, int transactionID, String operation) {
+        return lastUsedLSN + "," + transactionID + "," + operation;
     }
 
     public int retrieveLastUsedTransactionID() {
-        // TODO Implement the logic to retrieve the last used transaction ID from the log (if present)
-        // For example, you could read the log file and extract the last transaction ID from the entries
+        // TODO Implement the logic to retrieve the last used transaction ID from the
+        // log (if present)
+        // For example, you could read the log file and extract the last transaction ID
+        // from the entries
         int lastTransactionID = 0;
-    
+
         try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] entry = line.split(",");
                 int transactionID = Integer.parseInt(entry[0]);
-    
+
                 // Update the lastTransactionID if the current transactionID is greater
                 if (transactionID > lastTransactionID) {
                     lastTransactionID = transactionID;
@@ -77,7 +122,7 @@ public class RecoveryManager {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    
+
         return lastTransactionID;
     }
 
@@ -96,5 +141,34 @@ public class RecoveryManager {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void writeLogEntry(String entry) {
+        try {
+            FileWriter fileWriter = new FileWriter("log.txt", true);
+            fileWriter.write(entry + System.lineSeparator());
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int retrieveLastUsedLSN() {
+        int lastUsedLSN = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] entry = line.split(",");
+                int transactionID = Integer.parseInt(entry[0]);
+                // Update the lastTransactionID if the current transactionID is greater
+                if (transactionID > lastUsedLSN) {
+                    lastUsedLSN = transactionID;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lastUsedLSN;
     }
 }
